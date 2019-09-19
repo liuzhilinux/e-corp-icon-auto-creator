2816cabb76a26bc43df1a8353ca06f7fe9f84557
ling <i@e7iling.com>
2019-09-19 21:24:09 +0800

    一个错误的例子。

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
     let img = new Image();
 
     img.onload = function (ev) {
+        /*
+         (276, 0) {(275, 1), (277, 1)}
+
+         */
         let imgWidth = this.width;      // 505
         let imgHeight = this.height;    // 695
         imgHeight -= 170;               // 525
@@ -37,11 +41,23 @@
 
         let x = 0, y = 0;
         let tmpAhpla = 255, outlinePoints = [];
+        let tmpInPonits = [],
+            tmpOutPonits = [],
+            anglePoints = [],
+            tmpCnt = 0,
+            tmpType = '';
+
         let timer = setInterval(() => {
             let point = x + y * imgWidth;
             let ahpla = imgPixSet[point];
 
-            // console.log('(' + x + ', ' + y + ') => ' + ahpla);
+            let isIn = ahpla === 0;
+            let type = isIn ? 'in' : 'out';
+
+            if ('in' === tmpType && 'out' === type) tmpCnt++;
+            if (0 === x) tmpCnt = 0;
+
+            // console.log('(' + x + ', ' + y + ') => ' + ahpla + ' : ' + tmpCnt);
 
             if (tmpAhpla === ahpla && 0 === ahpla) {
                 newImageData.data[4 * point] = 255;
@@ -56,19 +72,67 @@
                 newImageData.data[4 * point - 2] = 0;
             }
 
+            if (!tmpInPonits[tmpCnt]) tmpInPonits[tmpCnt] = {x: 0, y: 0, c: '', type: ''};
+            if (!tmpOutPonits[tmpCnt]) tmpOutPonits[tmpCnt] = {x: 0, y: 0, c: '', type: ''};
+
             if (tmpAhpla !== ahpla) {
-                let isIn = ahpla === 0;
-                let type = isIn ? 'in' : 'out';
-                let points = {x: isIn ? x : x - 1, y, type};
-                console.log('(' + points.x + ', ' + points.y + ') => ' + type);
-                outlinePoints.push(points);
+                let point = {x: isIn ? x : x - 1, y, type};
+                let cStr = '';
+                outlinePoints.push(point);
+
+                let tmpPonit;
+
+                switch (type) {
+                    case 'in':
+                        tmpPonit = tmpInPonits[tmpCnt];
+                        break;
+                    case 'out':
+                        tmpPonit = tmpOutPonits[tmpCnt];
+                        break;
+                    default:
+                        break;
+                }
+
+                let tX = tmpPonit.x;
+                let tY = tmpPonit.y;
+                let tC = tmpPonit.c;
+
+                if (tY + 1 === y) {
+                    if (tX > x && '+' === tC) {
+                        anglePoints.push(tmpPonit);
+                        cStr = tmpPonit.c = '-';
+                    } else if (tX < x && '-' === tC) {
+                        anglePoints.push(tmpPonit);
+                        cStr = tmpPonit.c = '+';
+                    } else if ('*' === tC) {
+                        if (tX < x) {
+                            cStr = tmpPonit.c = '+';
+                        } else if (tX > x) {
+                            cStr = tmpPonit.c = '-';
+                        }
+                    } else if ('' === tC) {
+                        anglePoints.push({x, y, c: '*', type});
+                        cStr = tmpPonit.c = '*';
+                    }
+
+                    if ('in' === type) console.log(tmpPonit, tmpCnt);
+
+                    tmpPonit.x = x;
+                    tmpPonit.y = y;
+                    tmpPonit.type = type;
+                } else if (imgHeight === y) {
+                    anglePoints.push(point);
+                }
+
+                if ('in' === type) console.log('(' + point.x + ', ' + point.y + ') => ' + type + ' ' + cStr);
             }
 
             tmpAhpla = ahpla;
+            tmpType = type;
 
             ctx.putImageData(newImageData, 0, 0);
 
-            if (y + 1 >= imgHeight && x >= imgWidth) {
+            if (y + 1 >= imgHeight && x + 1 >= imgWidth) {
                 clearInterval(timer);
             } else if (x >= imgWidth) {
                 x = 0;
